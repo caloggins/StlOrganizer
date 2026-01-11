bfabed63bf295d8ef900955d5115b175d4b4d8f6
Christopher Loggins <christopher.loggins@gmail.com>
2026-01-11 12:16:37 -0600

    Test(FileSystem): Add unit tests for `FileSystemAdapter.GetDirectoryName`

---
+++ tests/StlOrganizer.Library.Tests/SystemFileAdapters/FileSystemAdapterTests.cs
@@ -0,0 +1,23 @@
+﻿using FakeItEasy;
+using Shouldly;
+using StlOrganizer.Library.SystemFileAdapters;
+
+namespace StlOrganizer.Library.Tests.SystemFileAdapters;
+
+public class FileSystemAdapterTests
+{
+    [Theory]
+    [InlineData(@"C:\MyFolder\File.zip", true, "MyFolder")]
+    [InlineData(@"C:\MyFolder\File.txt", true, "MyFolder")]
+    [InlineData(@"C:\MyFolder\target", false, "target")]
+    public void GetDirectoryName_WhenGivenPath_ReturnsCorrectResult(string path, bool exists, string expected)
+    {
+        var fileSystem = A.Fake<IFileOperations>();
+        A.CallTo(() => fileSystem.FileExists(path)).Returns(exists);
+        var sut = new FileSystemAdapter(fileSystem);
+
+        var output = sut.GetDirectoryName(path);
+        
+        output.ShouldBe(expected);
+    }
+}
