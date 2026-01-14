391decfcea487ed82011fb8791d7c1f40e9d6437
Christopher Loggins <christopher.loggins@gmail.com>
2026-01-14 05:42:46 -0600

    feat(decompression): Implement ZipFileAdapter using System.IO.Compression

---
+++ src/StlOrganizer.Library/Decompression/ZipFileAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System.IO.Compression;
+
+namespace StlOrganizer.Library.Decompression;
+
+public class ZipFileAdapter : IDecompressor
+{
+    public async Task DecompressAsync(
+        string file,
+        string folder,
+        CancellationToken cancellationToken)
+    {
+        await ZipFile.ExtractToDirectoryAsync(
+            file, folder, cancellationToken);
+    }
+}
