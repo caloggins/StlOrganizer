0ecd9e76885d07063811f591673a4168a7cf6e54
Christopher Loggins <christopher.loggins@gmail.com>
2026-01-14 05:42:46 -0600

    feat(decompression): Implement FolderScanner to find and delegate decompression

---
+++ src/StlOrganizer.Library/Decompression/FolderScanner.cs
@@ -0,0 +1,24 @@
+﻿using StlOrganizer.Library.SystemFileAdapters;
+
+namespace StlOrganizer.Library.Decompression;
+
+public class FolderScanner(
+    IFileSystem fileSystem,
+    IDecompressor decompressor) : IFolderScanner
+{
+    public async Task ScanAndDecompressAsync(
+        string folder,
+        CancellationToken cancellationToken = default)
+    {
+        var files = fileSystem.GetFiles(folder, "*.zip", SearchOption.AllDirectories)
+            .ToList();
+        
+        if (files.Count == 0)
+            throw new NoArchivesFoundException();
+        
+        foreach (var file in files)
+        {
+            await decompressor.DecompressAsync(file, folder, cancellationToken);
+        }
+    }
+}
