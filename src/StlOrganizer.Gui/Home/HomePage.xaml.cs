fb944e52a93a69a5d81a0a205122069e7442dcfa
Chris <chris@amberzoneventures.com>
2026-02-02 17:35:16 -0600

    feat: Add Home page UI and code-behind

---
+++ src/StlOrganizer.Gui/Home/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace StlOrganizer.Gui.Home;
+
+public partial class HomePage : Page
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
+
